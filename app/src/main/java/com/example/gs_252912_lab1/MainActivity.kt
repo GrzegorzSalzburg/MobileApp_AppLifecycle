108b996329a7f57189e9c1952e2421d454bcbb31
Grzegorz Salzburg <252912@student.pwr.edu.pl>
2022-03-06 20:35:13 +0100

    Added Comments

--- app/src/main/java/com/example/gs_252912_lab1/MainActivity.kt
+++ app/src/main/java/com/example/gs_252912_lab1/MainActivity.kt
@@ -1,56 +1,61 @@
 package com.example.gs_252912_lab1
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
 import android.widget.Toast
-import android.content.Context
-
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private var context: Context? = null        //new empty variable to Toast.makeText
+    private val duration = Toast.LENGTH_SHORT   //duration
 
-    private var context: Context? = null
-    private var duration = Toast.LENGTH_SHORT
-
-/*onCreate*/
-    override fun onCreate(savedInstanceState: Bundle?) {
+    /*onCreate*/
+    override fun onCreate(savedInstanceState: Bundle?) { //
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        context = applicationContext
-        Toast.makeText(context, "OnCreate",duration).show()
+        context = applicationContext                              //applicationContext to variable
+        Toast.makeText(context, "onCreate", duration).show() //Toast notification
     }
-/*onStart*/
-    override fun onStart(){
+
+    /*onStart*/
+    override fun onStart() { //
         super.onStart()
-        Toast.makeText(context, "onStart",duration).show()
+        Toast.makeText(context, "onStart", duration).show()
     }
-/*OnResume*/
-    override fun onResume(){
+
+    /*onResume*/
+    override fun onResume() {
         super.onResume()
-        Toast.makeText(context, "onResume",duration).show()
+        Toast.makeText(context,"onResume",duration).show()
     }
-/*onSaveInstanceState*/
+
+    /*onSaveInstanceState*/
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         Toast.makeText(context,"onSaveInstanceState",duration).show()
     }
-/*onPause*/
-    override fun onPause(){
+
+    /* onPause*/
+    override fun onPause() {
         super.onPause()
-        Toast.makeText(context, "onPause",duration).show()
+        Toast.makeText(context,"onPause",duration).show()
     }
-/*onStop*/
-    override fun onStop(){
+
+    /*onStop*/
+    override fun onStop() { //
         super.onStop()
-        Toast.makeText(context, "onStop",duration).show()
+        Toast.makeText(context,"onStop",duration).show()
     }
-/*onDestroy*/
+
+    /*onDestroy*/
     override fun onDestroy(){
         super.onDestroy()
-        Toast.makeText(context,"onDestroy",duration).show()
+        Toast.makeText(context, "onDestroy", duration).show()
     }
-/*onRestart*/
-    override fun onRestart(){
+
+    /*onRestart*/
+    override fun onRestart() {
         super.onRestart()
         Toast.makeText(context,"onRestart",duration).show()
     }
